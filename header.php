16e12252baa76ea631141230d5f76b8f547da410
Al Nemec <al.nemec@gmail.com>
2013-12-09 14:32:11 -0600

    Implemented SASS

--- header.php
+++ header.php
@@ -82,6 +82,7 @@
 	
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/main.css" />
 <!--[if IE]>
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/ie.css" />
 <![endif]-->
@@ -106,7 +107,7 @@
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-rotate.js"></script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/browserdetect.js"></script>
  
-  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/mainactions.js"></script>
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/min/mainactions.min.js"></script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/blurobjs.js"></script>
 
 <!--[if lt IE 9]>
