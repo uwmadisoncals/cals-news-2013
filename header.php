ba4ecc438963fc8d08f2a99f8c38dc8ce6891ea4
Al Nemec <al.nemec@gmail.com>
2013-12-13 14:37:41 -0600

    mobile styles

--- header.php
+++ header.php
@@ -83,6 +83,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/main.css" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/mobile.css" />
 <!--[if IE]>
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/ie.css" />
 <![endif]-->
